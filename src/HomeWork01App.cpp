87a24bb6af99b549df0d2334fe32f58225a379cf
Steven Angles <anglessw@muohio.edu>
2012-09-05 11:46:56 -0400

    CopyRectangle

--- src/HomeWork01App.cpp
+++ src/HomeWork01App.cpp
@@ -26,8 +26,20 @@ private:
 
 	void drawCircle(uint8_t* pixels, int x1, int y1, int radius, Color8u fill_color);
 
-	void copyRect(uint8_t* pixels);
+	void HomeWork01App::copyRect(uint8_t* pixels, int from_corner_x, int from_corner_y, int from_width, int from_height);
 };
+
+
+void HomeWork01App::copyRect(uint8_t* pixels, int from_corner_x, int from_corner_y, int from_width, int from_height)
+{
+	for(int y=from_corner_y; y<from_corner_y+from_height; y++) {
+		for(int x=from_corner_x; x<from_corner_x+from_width; x++) {
+			pixels[3*((x+from_height) + (y+from_width*kTextureSize))] = pixels[3*(x + y*kTextureSize)];
+			pixels[3*((x+from_height) + (y+from_width*kTextureSize)+1)] = pixels[3*(x + y*kTextureSize)+1];
+			pixels[3*((x+from_height) + (y+from_width*kTextureSize)+2)] = pixels[3*(x + y*kTextureSize)+2];
+		}
+	}
+}
 
 void HomeWork01App::drawCircle(uint8_t* pixels, int x1, int y1, int radius, Color8u fill_color)
 {
@@ -42,9 +54,9 @@ void HomeWork01App::drawCircle(uint8_t* pixels, int x1, int y1, int radius, Colo
 		for(int x=x_pos; x<x_distance; x++) {
 			point_distance = (((float) y - (float) y1)*((float) y - (float) y1)) + (((float) x - (float) x1)*((float) x - (float) x1));
 			if(point_distance <= radius*radius) {
-				pixels[3*(x + y*kTextureSize)] = c.r;
-				pixels[3*(x + y*kTextureSize)+1] = c.g;
-				pixels[3*(x + y*kTextureSize)+2] = c.b;
+				pixels[3*(x + y*kTextureSize)] = c.r;	//
+				pixels[3*(x + y*kTextureSize)+1] = c.g;	//Borrowed this from Brinkman's HW01
+				pixels[3*(x + y*kTextureSize)+2] = c.b;	//
 			}
 		}
 	}
@@ -66,9 +78,9 @@ void HomeWork01App::drawRectangle(uint8_t* pixels, int x1, int y1, int width, in
 		x_distance = x1 + width;
 	}
 
-	if(y1 < 0) {
-		y_pos = 0;
-		y_distance = height-(-1*y1);
+	if(y1 < 0) {						//
+		y_pos = 0;						// All these conditionals are for checking if the rectangle is within the window and fixing it if it's not
+		y_distance = height-(-1*y1);	//
 	}
 	else {
 		y_pos = y1;
@@ -84,9 +96,9 @@ void HomeWork01App::drawRectangle(uint8_t* pixels, int x1, int y1, int width, in
 
 	for(int y=y_pos; y<y_distance; y++) {
 		for(int x=x_pos; x<x_distance; x++) {
-			pixels[3*(x + y*kTextureSize)] = c.r;
-			pixels[3*(x + y*kTextureSize)+1] = c.g;
-			pixels[3*(x + y*kTextureSize)+2] = c.b;
+			pixels[3*(x + y*kTextureSize)] = c.r;	//
+			pixels[3*(x + y*kTextureSize)+1] = c.g;	//Borrowed from Brinkman's HW01
+			pixels[3*(x + y*kTextureSize)+2] = c.b;	//
 		}
 	}
 }
@@ -108,6 +120,8 @@ void HomeWork01App::update()
 
 	drawCircle(dataArray, 150, 100, 50, Color8u(0,255,0));
 	drawCircle(dataArray, 150, 350, 50, Color8u(0,255,0));
+
+	//copyRect(dataArray, 0, 0, 400, 600);
 }
 
 void HomeWork01App::draw()
